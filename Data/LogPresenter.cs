92e946fd976b86df3ec419ea75cc4b333a521d73
lkkg18 <lkkg18@msn.com>
2019-12-05 13:13:57 +0800

    加入專案檔案。

---
+++ Data/LogPresenter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlazorMemoryTest.Data
+{
+    public class LogPresenter
+    {
+        public DateTime Occurs { get; } = DateTime.Now;
+        public string Message { get; set; } = "";
+        public string Source { get; set; } = "SYSTEM";
+
+        public override string ToString() => $"{Occurs:hh:MM:ss (fff)}{Message}{Source}";
+    }
+}
---
+++ Data/LogService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorMemoryTest.Data
+{
+    public class LogService
+    {
+        private ConcurrentQueue<LogPresenter> presentQueue { get; } = new ConcurrentQueue<LogPresenter>();
+
+        public event EventHandler<LogPresenter> OnLog = null;
+        public List<LogPresenter> Logs { get; } = new List<LogPresenter>();
+        public string Filter { get; set; } = string.Empty;
+
+        public LogService()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    SpinWait.SpinUntil(() => false, 10);
+                    if (!presentQueue.TryDequeue(out var item)) { continue; }
+
+                    if (Logs.Count > 500)
+                    {
+                        var temp = Logs.Take(50);
+                        Logs.Clear();
+                        Logs.AddRange(temp);
+                    }
+
+                    Logs.Insert(0, item);
+                    OnLog?.Invoke(this, item);
+                }
+            });
+        }
+
+        public IEnumerable<LogPresenter> GetLog() => string.IsNullOrEmpty(Filter) ? Logs : Logs.Where(o => o.ToString().ToLower().Contains(Filter))
+                                                                                               .Take(500);
+
+        public void PushMessage(string message)
+        {
+            presentQueue.Enqueue(new LogPresenter()
+            {
+                Message = message,
+            });
+        }
+    }
+}
